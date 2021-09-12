51a12fffd2d1a071dbb74957e836385af18be894
dejun teng <tengdj@vip.qq.com>
2021-09-12 15:28:27 -0400

    update

--- src/name/Namer.java
+++ src/name/Namer.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class Namer {
 	
 	public static void main(String args[]) {
-		
-		ArrayList<Phase> phases = new ArrayList<Phase>();
-		SishuwujingLoader ssloader = new SishuwujingLoader();
-		phases.addAll(ssloader.load("reference/sishuwujing.txt"));
-		ChuciLoader ccloader = new ChuciLoader();
-		phases.addAll(ccloader.load("reference/chuci.txt"));
+//		
+//		ArrayList<Phase> phases = new ArrayList<Phase>();
+//		SishuwujingLoader ssloader = new SishuwujingLoader();
+//		phases.addAll(ssloader.load("reference/sishuwujing.txt"));
+//		ChuciLoader ccloader = new ChuciLoader();
+//		phases.addAll(ccloader.load("reference/chuci.txt"));
 //		ShijiLoader sjloader = new ShijiLoader();
 //		phases.addAll(sjloader.load("reference/shiji.txt"));
 //		GlobalLoader zjtjloader = new GlobalLoader("资治通鉴");
@@ -19,20 +19,27 @@ public class Namer {
 //		phases.addAll(tsloader.load("reference/tangshi.txt"));
 //		GlobalLoader scloader = new GlobalLoader("全宋词");
 //		phases.addAll(scloader.load("reference/songci.txt"));
-		Query q = new Query();
-		//q.turntowuxing();
-		//q.setOutput("C:\\Users\\admin\\Desktop\\name.txt");
-		//q.sethuo();
-		//q.setShui();
-		//q.setJin();
-		q.setTu();
-		//q.setMu();
-		//q.addTarget("婉清");
-		q.addTarget("清");
-		q.query(phases);
-
-//		Query q = new Query('书', "上尚诏曰");
-//		q.query(phases);		
+//		Query q = new Query();
+//		//q.turntowuxing();
+//		//q.setOutput("C:\\Users\\admin\\Desktop\\name.txt");
+//		//q.sethuo();
+//		//q.setShui();
+//		//q.setJin();
+//		//q.setTu();
+//		//q.setMu();
+//		//q.addTarget("婉清");
+//		q.addTarget("佳惠");
+//		q.query(phases);
+//
+////		Query q = new Query('书', "上尚诏曰");
+////		q.query(phases);	
+		
+		for(int i=1;i<100000;i++) {
+			double v = i*0.86243549292146;
+			if(v-(int)v<0.0001) {
+				System.out.println(i+" "+v);
+			}
+		}
 	}
 	
 }
