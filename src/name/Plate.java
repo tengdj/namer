196b135e5f6f1d93caedb08b756c26f25dca1a8f
Dejun Teng <tengdj@vip.qq.com>
2022-07-07 11:18:46 +0800

    Update Plate.java

--- src/name/Plate.java
+++ src/name/Plate.java
@@ -23,113 +23,113 @@ public class Plate {
 		ArrayList<String> contained = new ArrayList<String>();
 //		contained.add("103");
 //		contained.add("127");
-//		contained.add("312");
-//		contained.add("208");
-//		contained.add("315");
+		contained.add("312");
+		contained.add("208");
+		contained.add("315");
 //		contained.add("BJH");
-		boolean keep_seq = false;
+		boolean keep_seq = true;
 		boolean allow_n = false;
 		for(String s:lines) {
 			
-			boolean valid = false;
-			for(int i=0;i<s.length();i++) {
-				int c = 0;
-				for(int j=0;j<s.length();j++) {
-					if(s.charAt(i)==s.charAt(j)) {
-						c++;
+//			boolean valid = false;
+//			for(int i=0;i<s.length();i++) {
+//				int c = 0;
+//				for(int j=0;j<s.length();j++) {
+//					if(s.charAt(i)==s.charAt(j)) {
+//						c++;
+//					}
+//				}
+//				if(c>=3) {
+//					valid = true;
+//				}
+//			}
+//			if(valid)
+//			System.out.println(s);
+
+			/* must follow some pattern */
+			if(allowed_style.size()>0) {
+				boolean isallowed = false;
+				for(String a:allowed_style) {
+					boolean al = true;
+					for(int i=0;i<a.length();i++) {
+						if(a.charAt(i)=='L'&&(s.charAt(i)>='0'&&s.charAt(i)<='9')) {
+							al = false;
+							break;
+						}
+						if(a.charAt(i)=='N'&&(s.charAt(i)<'0'||s.charAt(i)>'9')) {
+							al = false;
+							break;
+						}
+					}
+					if(al) {
+						isallowed = true;
 					}
 				}
-				if(c>=3) {
-					valid = true;
+				if(!isallowed) {
+					continue;
 				}
 			}
-			if(valid)
+			
+			/* skip some pattern */
+			if(skipped.size()>0) {
+				boolean issk = false;
+				for(String sk:skipped) {
+					if(s.contains(sk)) {
+						issk = true;
+						break;
+					}
+				}
+				if(issk) {
+					continue;
+				}
+			}
+			
+			/* at least some number of numbers*/
+			if(min_number>0) {			
+				int nc = 0;
+				for(int j=0;j<s.length();j++) {
+					if(s.charAt(j)>='0'&&s.charAt(j)<='9'){
+						nc++;
+					}
+				}
+				//System.out.println(nc);
+				if(nc<min_number) {
+					continue;
+				}
+			}
+			
+			
+			/* must contain the query */
+			if(contained.size()>0) {
+				boolean valid = false;
+				for(String query:contained) {
+					if(keep_seq){
+						if(s.contains(query)) {
+							valid = true;
+							break;
+						}
+					}else {
+						int cnum = 0;
+						for(int i=0;i<query.length();i++) {
+							for(int j=0;j<s.length();j++) {
+								if(query.charAt(i)==s.charAt(j)) {
+									cnum++;
+									break;
+								}
+							}
+						}
+						if(cnum==query.length()) {
+							valid = true;
+							break;
+						}
+					}
+				}
+				if(!valid) {
+					continue;
+				}
+			}
+			
 			System.out.println(s);
-
-//			/* must follow some pattern */
-//			if(allowed_style.size()>0) {
-//				boolean isallowed = false;
-//				for(String a:allowed_style) {
-//					boolean al = true;
-//					for(int i=0;i<a.length();i++) {
-//						if(a.charAt(i)=='L'&&(s.charAt(i)>='0'&&s.charAt(i)<='9')) {
-//							al = false;
-//							break;
-//						}
-//						if(a.charAt(i)=='N'&&(s.charAt(i)<'0'||s.charAt(i)>'9')) {
-//							al = false;
-//							break;
-//						}
-//					}
-//					if(al) {
-//						isallowed = true;
-//					}
-//				}
-//				if(!isallowed) {
-//					continue;
-//				}
-//			}
-//			
-//			/* skip some pattern */
-//			if(skipped.size()>0) {
-//				boolean issk = false;
-//				for(String sk:skipped) {
-//					if(s.contains(sk)) {
-//						issk = true;
-//						break;
-//					}
-//				}
-//				if(issk) {
-//					continue;
-//				}
-//			}
-//			
-//			/* at least some number of numbers*/
-//			if(min_number>0) {			
-//				int nc = 0;
-//				for(int j=0;j<s.length();j++) {
-//					if(s.charAt(j)>='0'&&s.charAt(j)<='9'){
-//						nc++;
-//					}
-//				}
-//				//System.out.println(nc);
-//				if(nc<min_number) {
-//					continue;
-//				}
-//			}
-//			
-//			
-//			/* must contain the query */
-//			if(contained.size()>0) {
-//				boolean valid = false;
-//				for(String query:contained) {
-//					if(keep_seq){
-//						if(s.contains(query)) {
-//							valid = true;
-//							break;
-//						}
-//					}else {
-//						int cnum = 0;
-//						for(int i=0;i<query.length();i++) {
-//							for(int j=0;j<s.length();j++) {
-//								if(query.charAt(i)==s.charAt(j)) {
-//									cnum++;
-//									break;
-//								}
-//							}
-//						}
-//						if(cnum==query.length()) {
-//							valid = true;
-//							break;
-//						}
-//					}
-//				}
-//				if(!valid) {
-//					continue;
-//				}
-//			}
-//			
-//			System.out.println(s);
 		}
 		System.out.println("completed");
 	}
